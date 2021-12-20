7d95c583cd49a4657c529fa3714ce12eb9a6df7d
JWo1f28 <jwearing2806@gmail.com>
2021-12-20 15:17:31 +0000

    Logging Changes

--- scansub.bat
+++ scansub.bat
@@ -4,11 +4,9 @@ echo scansub-%1-3: Get IP from Passed Var >> log.txt
 copy nul %ip%.txt 1>>log.txt
 echo scansub-%1-5: Create IP txt file >> log.txt
 for /l %%a in (1,1,1024) DO (
-    echo scansub-%1-7: Create FTP command file >> log.txt
     echo open %ip% %%a > %ip%.txt
     echo quit >> %ip%.txt
-    echo scansub-%1-10: Use FTP to test Port Status >> log.txt
-    ftp -s:%ip%.txt 2>&1 | find "host" >>log.txt
+    ftp -s:%ip%.txt 2>&1 | find "host" >nul
     if not errorlevel 1 (
         echo %ip% %%a >> ports.txt
     )

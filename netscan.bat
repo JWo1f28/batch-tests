4a1c8e691423ef1c79515d47f121f22c40447c0d
JWo1f28 <96420649+JWo1f28@users.noreply.github.com>
2021-12-20 14:07:06 +0000

    Netscan Function

--- netscan.bat
+++ netscan.bat
@@ -1,32 +1,26 @@
 @echo off
-echo call netscan.bat end>> log.txt
 
 ::NETSCAN FUNCTION
 cls
 setlocal enabledelayedexpansion
 
 ::Variables
-echo counter variable set >> log.txt
 Set _counter=0
 set _domain=192.168
 
 ::Refresh ipadr.txt
-echo refresh ipadr.txt >> log.txt
 del ipadr.txt 2>nul
 copy nul ipadr.txt 1>nul
 echo --ONLINE IPv4 DEVICES-- >> ipadr.txt
 
 ::Network Probe
-echo start net probe >> log.txt
 echo STARTING IPv4 NETWORK PROBE, THIS WILL TAKE A WHILE
 ::Scan all Local IPs
 for /L %%a IN (0,1,255) DO (
     for /L %%b IN (0,1,255) DO (
-        echo testing %_domain%.%%a.%%b >> log.txt
         set _toping=%_domain%.%%a.%%b
         ping -n 1 !_toping! | find "TTL" >nul
         if not errorlevel 1 (
-            echo ipadr online, starting nslookup >> log.txt
             nslookup !_toping! 2>&1 | find "Name" >nul
             if errorlevel 1 (
                 set _iptag=NO DOMAIN
@@ -35,11 +29,9 @@ for /L %%a IN (0,1,255) DO (
                     set _iptag=%%c
                 )
             )
-            echo send nslookup domain name to ipadr.txt >> log.txt
             echo !_toping! - !_iptag! >> ipadr.txt
         )
         Rem Progress
-        echo user feedback start >> log.txt
         set /a _counter+=1
         for /f %%f in ('copy /Z "%~dpf0" nul') do set "CR=%%f"
         for /f %%f in ('"prompt $H&for %%g in (0) do rem"') do set "BS=%%f"
@@ -49,7 +41,6 @@ for /L %%a IN (0,1,255) DO (
    
 
 cls
-echo netscan complete, return to menu>> log.txt
 echo SCAN COMPLETE, CONTINUE TO RETURN TO THE MENU
 
 pause
